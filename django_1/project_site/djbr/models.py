debe9cb851e5dff68caa7f832b5abdb0acc87bae
Panos Louridas <louridas@gmail.com>
2016-10-11 16:09:24 +0300

    import into repo

---
+++ django_1/project_site/djbr/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+import datetime
+from django.utils import timezone
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    pub_year = models.IntegerField('year published', default=2000)
+
+
+    def was_published_recently(self):
+        return self.pub_year >= timezone.now().year - 1
+    
+    def __str__(self):
+        return "%s %s" % (self.title, self.pub_year)
+    
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField(default="")
+    review_date = models.DateTimeField('review date')
+
+    def __str__(self):
+        return "%s %s" % (self.title, self.review_date)
+
---
+++ django_1/project_site/project_site/site_config.py
@@ -0,0 +1,11 @@
+SECRET_KEY = '#8*xfqx#w(k@*fr9i=50i1k&wt2xr1b5k!f9%rq@^ewb2%l69*'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djbr',
+        'USER': 'djbr_user',
+        'PASSWORD': 'g8nzmktk6y',
+        'HOST': '127.0.0.1',
+    }
+}
