e7050f93ad1222bc9037d1705602a7235af9bf38
Panos Louridas <louridas@gmail.com>
2016-12-12 18:23:53 +0200

    new module and corrections

--- nodejs_express/bangular_node/routes/books-router.js
+++ nodejs_express/bangular_node/routes/books-router.js
@@ -3,79 +3,54 @@ const router = express.Router();
 
 const Book = require('../models/book');
 
-router.route('/books')
-  .get(function(req, res) {
-    if (req.query.title) {
-      Book.find({
-        title: new RegExp(req.query.title)
-      }, function(err, books) {
-        if (err) {
-          res.send(err);
-        }
-        res.json(books);
-      });
+router.get('/books', function(req, res) {
+  var query = Book.find({});
+  if (req.query.title) {
+    query.where('title').equals(new RegExp(req.query['title']));
+  }
+  query.exec(function(err, books) {
+    if (err) {
+      res.send(err);
     } else {
-      Book.find(function(err, books) {
-        if (err) {
-          res.send(err);
-        }
-        res.json(books);
-      });
+      res.json(books);
     }
   });
+});
 
-router.route('/books')
-  .post(function(req, res) {
-    var book = new Book();
-    book.title = req.body.title;
-    book.pub_year = req.body.pub_year;
-    
-    book.save(function(err) {
-      if (err) {
-        res.send(err);
-      }
+router.post('/books', function(req, res) {
+  var book = new Book();
+  book.title = req.body.title;
+  book.pub_year = req.body.pub_year;
+  book.save(function(err) {
+    if (err) {
+      res.send(err);
+    } else {
       res.json(book);
-    });
+    }
   });
+});
 
-router.route('/books/:book_id')
-  .get(function(req, res) {
-    Book.findById(req.params.book_id, function(err, book) {
-      if (err) {
-        res.send(err);
-      }
+router.get('/books/:book_id', function(req, res) {
+  Book.findById(req.params.book_id, function(err, book) {
+    if (err) {
+      res.send(err);
+    } else {
       res.json(book);
-    });
+    }
   });
+});
 
-router.route('/books/:book_id')
-  .put(function(req, res) {
-    Book.findById(req.params.book_id, function(err, book) {
-      if (err) {
-        res.send(err);
-      }
-      book.title = req.body.title;
-      book.pub_year = req.body.pub_year;
-      book.save(function(err) {
-        if (err) {
-          res.send(err);
-        }
-        res.json(book);
-      });
-    });
-  });
-
-router.route('/books/:book_id').
-  delete(function(req, res) {
-    Book.remove({
-      _id: req.params.book_id
-    }, function(err, result) {
-      if (err) {
-        res.send(err);
-      }
+router.delete('/books/:book_id', function(req, res) {
+  Book.remove({
+    _id: req.params.book_id
+  }, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
       res.json(result);
-    });
+    }
   });
+});
 
 module.exports = router;
 
