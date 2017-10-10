9424a27e311e5a612743f26ca0c831778c315e2c
Panos Louridas <louridas@gmail.com>
2017-10-10 18:46:36 +0300

    update

--- django_2/project_site/djbr/urls.py
+++ django_2/project_site/djbr/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^book/(?P<book_id>[0-9]+)/$', views.book, name='book'),
     url(r'^book/(?P<book_id>[0-9]+)/reviews/$', views.reviews, name='reviews'),
-    url(r'^author/(?P<author_id>[0-9]+)/$', views.author, name='author'),
+    url(r'^author/(?P<author_id>[0-9]+)/$', views.author, name='author'),    
 ]
