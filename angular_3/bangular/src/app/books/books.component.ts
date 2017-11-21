99005a4f21ac34fd68064e67d93218238aa6bf0c
Panos Louridas <louridas@gmail.com>
2017-11-21 11:07:02 +0200

    add forgotten directory

---
+++ angular_3/bangular/src/app/books/books.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Book } from '../book';
+import { BookService } from '../book.service';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css'],
+})
+export class BooksComponent implements OnInit {
+
+  books: Book[];
+  selectedBook : Book;
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit() {
+    this.getBooks();
+  }
+
+  onSelect(book: Book): void {
+    this.selectedBook = book;
+  }
+
+  getBooks(): void {
+    this.bookService.getBooks().then(books => {
+      this.books = books;
+    });
+  }
+
+}
